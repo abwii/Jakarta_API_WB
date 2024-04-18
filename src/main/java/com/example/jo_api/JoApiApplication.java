d323b64a1e0ff5e8f02cbbd3a3c4c825387958d2
Wassim <wassim.bacha@adventori.com>
2024-04-18 11:01:59 +0200

    first commit

---
+++ src/main/java/com/example/jo_api/JoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoApiApplication.class, args);
+    }
+
+}
